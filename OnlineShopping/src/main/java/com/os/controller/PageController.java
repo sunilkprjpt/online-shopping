cfe0eaa955fec5d9692c948fb461b4de97028d86
sunil <sunil@DESKTOP-EH15K11>
2020-02-02 16:13:08 +0530

    Add new jsp file and controller mapping about, contact

--- OnlineShopping/src/main/java/com/os/controller/PageController.java
+++ OnlineShopping/src/main/java/com/os/controller/PageController.java
@@ -6,13 +6,31 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	
-	@RequestMapping(value= {"/", "/home", "/index"})
-	public ModelAndView index()
-	{
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
 		ModelAndView mav = new ModelAndView("page");
-		mav.addObject("greeting","Welcome to spring mvc");
-		
+		mav.addObject("title", "Home");
+		mav.addObject("userClickHome", true);
+
+		return mav;
+	}
+
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mav = new ModelAndView("page");
+		mav.addObject("title", "About Us");
+		mav.addObject("userClickAbout", true);
+
+		return mav;
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mav = new ModelAndView("page");
+		mav.addObject("title", "Contact Us");
+		mav.addObject("userClickContact", true);
+
 		return mav;
 	}
 
